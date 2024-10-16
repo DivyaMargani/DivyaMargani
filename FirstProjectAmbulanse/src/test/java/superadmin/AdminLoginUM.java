8e9fc1baf6d85d7d611ab2882ca46281b49a1f56
divya <divyavanimargani@gmail.com>
2024-10-16 14:00:59 +0530

    This is changed commit

--- FirstProjectAmbulanse/src/test/java/superadmin/AdminLoginUM.java
+++ FirstProjectAmbulanse/src/test/java/superadmin/AdminLoginUM.java
@@ -27,6 +27,7 @@ public class AdminLoginUM {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
 		driver.get("https://ambulance.genamplifysol.com/");
+		System.out.println("Hello");
 	}
 
 	@Test()
